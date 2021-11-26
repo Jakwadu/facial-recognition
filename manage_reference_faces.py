bde2c6c52924d03c897dd38f783cf62fd8224c65
Jakwadu <44432334+Jakwadu@users.noreply.github.com>
2021-11-26 13:41:27 +0000

    Added 'create_database_from_references' to manage_reference_faces.py to build sqlite database from reference images

--- manage_reference_faces.py
+++ manage_reference_faces.py
@@ -1,8 +1,15 @@
 import os
+import io
+import sqlite3
+import pandas as pd
+import pickle
+from face_detector import FaceDetector
+from face_encoder import FaceEncoder
+from face_dataset_encoder import read_and_encode_images
 from argparse import ArgumentParser
 from shutil import copy, rmtree
 
-DATABASE_DIR = 'references'
+REFERENCE_DIR = 'references'
 
 
 def build_parser():
@@ -10,14 +17,15 @@ def build_parser():
     p.add_argument('-p', '--person', type=str, dest='person', help='The name of the person to be added/removed')
     p.add_argument('-a', '--add', type=str, dest='add', help='Add person to database')
     p.add_argument('-r', '--remove', dest='remove', action='store_true', help='Remove person from database')
+    p.add_argument('-b', '--build-database', dest='build_database', action='store_false')
     return p
 
 
-def create_new_database_entry(name, source_directory):
+def create_new_reference(name, source_directory):
     files = os.listdir(source_directory)
     img_files = [os.path.join(source_directory, f) for f in files if f[-4:] == '.jpg']
     assert len(img_files) > 0, 'No .jpg files found in the specified directory'
-    new_directory = os.path.join(DATABASE_DIR, name)
+    new_directory = os.path.join(REFERENCE_DIR, name)
     os.makedirs(new_directory)
     for img_file in img_files:
         f = os.path.basename(img_file)
@@ -26,7 +34,7 @@ def create_new_database_entry(name, source_directory):
 
 
 def add_person_to_database(name, source_directory):
-    if os.path.exists(os.path.join(DATABASE_DIR, name)):
+    if os.path.exists(os.path.join(REFERENCE_DIR, name)):
         answer = input(f'{name} already exists in the database. Do yo want to replace the old entry?[y/n]:')
         check = [answer == char for char in ['y', 'Y', 'n', 'N']]
         if not any(check):
@@ -35,21 +43,21 @@ def add_person_to_database(name, source_directory):
             if answer == 'n' or answer == 'N':
                 exit()
             else:
-                remove_person_from_database(name)
-                create_new_database_entry(name, source_directory)
+                remove_person_from_references(name)
+                create_new_reference(name, source_directory)
     else:
-        create_new_database_entry(name, source_directory)
+        create_new_reference(name, source_directory)
 
 
-def remove_person_from_database(name):
-    path = os.path.join(DATABASE_DIR, name)
+def remove_person_from_references(name):
+    path = os.path.join(REFERENCE_DIR, name)
     assert os.path.exists(path), f'{name} was not found. It may have already been removed'
     rmtree(path)
     print(f'{name} has been removed from the database')
 
 
-def list_people_in_database():
-    people = os.listdir(DATABASE_DIR)
+def list_people_in_references():
+    people = os.listdir(REFERENCE_DIR)
     space = ' '*30
     header0 = 'Name'
     header1 = 'Number of Reference Faces'
@@ -58,8 +66,22 @@ def list_people_in_database():
     print('-'*(len(header0) + len(space) + len(header1)))
     for p in people:
         name = p + space[:-len(p)]
-        n_faces = len(os.listdir(os.path.join(DATABASE_DIR, p)))
+        n_faces = len(os.listdir(os.path.join(REFERENCE_DIR, p)))
         print(name + str(n_faces))
+
+
+def create_database_from_references():
+    db_connection = sqlite3.connect('face_embeddings.db')
+    face_detector = FaceDetector(n_faces=1, face_size=(224, 224))
+    face_encoder = FaceEncoder(encoder_type='vgg-face')
+    dataframes = []
+    for name in os.listdir(REFERENCE_DIR):
+        embeddings = read_and_encode_images(os.path.join(REFERENCE_DIR, name), face_detector, face_encoder)
+        for embedding in embeddings:
+            dataframes.append(pd.DataFrame({'name': [name], 'embedding': [embedding.tobytes()]}))
+    df = pd.concat(dataframes)
+    print(df.head())
+    df.to_sql('face_embeddings', con=db_connection, index=False, if_exists='replace')
 
 
 if __name__ == '__main__':
@@ -72,6 +94,8 @@ if __name__ == '__main__':
         add_person_to_database(person, src_directory)
     elif arguments.remove:
         assert person, 'Name of person to be removed not specified'
-        remove_person_from_database(person)
+        remove_person_from_references(person)
+    elif arguments.build_database:
+        create_database_from_references()
     else:
-        list_people_in_database()
+        list_people_in_references()
