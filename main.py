14a04718f604fcf4ab62892c2fe8662f90640152
Jakwadu <44432334+Jakwadu@users.noreply.github.com>
2021-12-02 10:29:01 +0000

    Updated the parser to make the system more configurable

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from facial_recognition_utils import recognise_face_in_image, recognise_face_in_
 
 DEFAULT_REFERENCE_FACE_PATH = 'references'
 SIMILARITY_THRESHOLD = 0.6
-USE_MTCNN = False
-USE_FACIAL_ALIGNMENT = True
 
 
 def build_parser():
     parser = ArgumentParser()
 
     parser.add_argument('-i', '--image', type=str,
-                        dest='image', help='Path to file if doing facial recognition on an image',
-                        metavar='IMAGE', required=False)
+                        dest='image', help='Path to file if doing facial recognition on an image', required=False)
 
-    parser.add_argument('-r', '--reference-faces', type=str,
-                        dest='reference_faces', help='Directory containing examples of the person of interest',
-                        metavar='REFERENCE_FACES', required=False, default=DEFAULT_REFERENCE_FACE_PATH)
+    parser.add_argument('-r', '--reference-faces', type=str, dest='reference_faces',  required=False,
+                        default=DEFAULT_REFERENCE_FACE_PATH,
+                        help='Directory containing examples of the person of interest')
+
+    parser.add_argument('-m', '--mtcnn', dest='mtcnn', action='store_true', help='Use MTCNN for face detection')
+
+    parser.add_argument('-t', '--threshold', type=float, dest='threshold', required=False, default=SIMILARITY_THRESHOLD,
+                        help='Euclidean distance threshold for image similarity')
+
+    parser.add_argument('-n', '--no-alignment', dest='no_alignment', action='store_true',
+                        help='Disable facial alignment after face detection')
 
     return parser
 
@@ -26,11 +31,10 @@ def build_parser():
 if __name__ == '__main__':
     arg_parser = build_parser()
     args = arg_parser.parse_args()
-    reference_faces = args.reference_faces
-    recogniser = FacialRecogniser(reference_faces,
-                                  threshold=SIMILARITY_THRESHOLD,
-                                  align=USE_FACIAL_ALIGNMENT,
-                                  mtcnn=USE_MTCNN)
+    recogniser = FacialRecogniser(args.reference_faces,
+                                  threshold=args.threshold,
+                                  align=not args.no_alignment,
+                                  mtcnn=args.mtcnn)
     image_path = args.image
     if not image_path:
         print(f'***** Running facial recognition on video stream')
